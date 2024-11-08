136ba38ca704fbeb30c92c48fb3af3b6fcde349e
sheng232 <lowellchessofficial@gmail.com>
2024-11-08 00:24:57 -0800

    added a flip function with state

--- Flash-Card-Project/src/components/Flashcard.jsx
+++ Flash-Card-Project/src/components/Flashcard.jsx
@@ -7,16 +7,16 @@ function Flashcard(props){
             return !prevState;
         })
     }
-
+    
     return(
         <div className="container flexbox">
             <div className="flashcard flexbox">
-                <p className={isFront && "front-text"}>{props.frontText}</p>
-                <p className={"back-text"}>{props.backText}</p>
+                <p className={`front-text ${!isFront ? "invisible" : ""}`}>{props.frontText}</p>
+                <p className={`back-text ${isFront ? "invisible" : ""}`}>{props.backText}</p>
             </div>
             <div className="inputs">
                 <button className="previous">Previous</button>
-                <button className="show-answer" onClick={flipCard}>Show Answer</button>
+                <button className="show-answer" onClick={flipCard}>{isFront ? "Show Answer" : "Hide Answer"}</button>
                 <button className="next">Next</button>
             </div>
         </div>
